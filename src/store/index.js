0c8051d1d5b3fde69193c45f4c2fecf00dae08be
okzaha <okzaha456@hotmail.com>
2021-02-24 22:01:13 +0700

    งานใหม่อาจารย์โจ.EXE

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,84 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    click1: 0,
+    click2: 0,
+    click3: 0,
+    click4: 0,
+    click5: 0,
+    click6: 0,
+    click7: 0
+  },
+  mutations: {
+    click11: state => {
+      return state.click1++, (state.click7 += 24);
+    },
+    click22: state => {
+      return state.click2++,(state.click7 += 15);
+    },
+    click33: state => {
+      return state.click3++,(state.click7 += 24);
+    },
+    click44: state => {
+      return state.click4++,(state.click7 += 24);
+    },
+    click55: state => {
+      return state.click5++,(state.click7 += 15);
+    },
+    click66: state => {
+      return state.click6++, (state.click7 += 24);
+    },
+    click111: state => {
+      return (state.click1 = 0),
+        (state.click2 = 0),
+        (state.click3 = 0),
+        (state.click4 = 0),
+        (state.click5 = 0),
+        (state.click6 = 0),
+        (state.click7 = 0);
+    },
+    click121: state => {
+      if (state.click1 > 0) {
+        return state.click1--,(state.click7 -= 24);
+      }
+    },
+    click122: state => {
+      if (state.click2 > 0) {
+        return state.click2--,(state.click7 -= 15);
+      }
+    },
+    click123: state => {
+      if (state.click3 > 0) {
+        return state.click3--,(state.click7 -= 24);
+      }
+    },
+    click124: state => {
+      if (state.click4 > 0) {
+        return state.click4--,(state.click7 -= 24);
+      }
+    },
+    click125: state => {
+      if (state.click5 > 0) {
+        return state.click5--,(state.click7 -= 15);
+      }
+    },
+    click126: state => {
+      if (state.click6 > 0) {
+        return state.click6--,(state.click7 -= 24);
+      }
+    },
+    click1111: state => {
+      if (state.click7 > 0) {
+        alert(
+          "ขอบคุณที่ใช้บริการ สินค้าที่คุณลูกค้าสั่งนั้นจะถูกจัดส่งให้ในเร็วๆนี้ "
+        );
+      }
+      else if (state.click7 <= 0) {
+        alert(
+          "กลุนาเลือกสินค้าก่อน "
+        );
+      }
+    }
+  }
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,71 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <div class="p-4">
+    <b-icon icon="house-door-fill" animation="spin"  ></b-icon> รายการสินค้า <b-icon icon="house-door-fill" animation="spin"  ></b-icon>  
+  
+      <b-row>
+
+ 
+  
+<b-col>
+    <p>Artoria Pendragon <code>24$</code>:</p>
+  <b-button class="p-3 mb-2 bg-white text-dark" @click="$store.commit('click11')" >
+    <b-img src="https://appmedia.jp/wp-content/uploads/2016/06/c5f3771c96a6c0ed7070e5f63bd0214d.jpg" width="170" ></b-img></b-button>
+   <b-col ><b-button size="sm" variant="danger" @click="$store.commit('click11')" >ลงรถเข็น <b-badge variant="light">{{$store.state.click1}}</b-badge></b-button>  <b-button size="sm" variant="danger" @click="$store.commit('click121')" >เอาออกรถเข็น </b-button>
+   </b-col>
+</b-col>
+<b-col>
+  <p>Artoria Pendragon (Alter) <code>15$</code>:</p>
+  <b-button class="p-3 mb-2 bg-white text-dark" @click="$store.commit('click22')" >
+    <b-img src="https://www.wicurio.com/grand_order/index.php?plugin=resizeimage&page=%E3%82%A8%E3%82%A4%E3%83%97%E3%83%AA%E3%83%AB%E3%83%95%E3%83%BC%E3%83%AB%EF%BC%88H28%EF%BC%89&w=163&h=280&image=0034128.jpg" width="170"></b-img></b-button>
+ <b-col ><b-button size="sm" variant="danger" @click="$store.commit('click22')" >ลงรถเข็น 
+   <b-badge variant="light">{{$store.state.click2}}</b-badge></b-button>  <b-button size="sm" variant="danger" @click="$store.commit('click122')" >เอาออกรถเข็น </b-button>
+   </b-col></b-col>
+<b-col>
+  <p>Okita Sōji <code>24$</code>:</p>
+  <b-button class="p-3 mb-2 bg-white text-dark" @click="$store.commit('click33')" >
+    <b-img src="https://i.pinimg.com/236x/34/f5/2f/34f52fdbfa15318e91201feda7a2b6ab.jpg" width="170" ></b-img></b-button>
+ <b-col ><b-button size="sm" variant="danger" @click="$store.commit('click33')" >ลงรถเข็น
+    <b-badge variant="light">{{$store.state.click3}}</b-badge></b-button>  <b-button size="sm" variant="danger" @click="$store.commit('click123')" >เอาออกรถเข็น </b-button>
+   </b-col>
+   </b-col>
+   </b-row>
+   <br>
+   <b-row>
+  <b-col>
+    <p>Astolfo (Saber) <code>24$</code>:</p>
+ <b-button class="p-3 mb-2 bg-white text-dark" @click="$store.commit('click44')" >
+    <b-img src="https://appmedia.jp/wp-content/uploads/2020/03/b4a5b2165a9271cac91761300c74376e.jpg" width="170"></b-img></b-button>
+   <b-col ><b-button size="sm" variant="danger" @click="$store.commit('click44')" >ลงรถเข็น 
+     <b-badge variant="light">{{$store.state.click4}}</b-badge></b-button>  <b-button size="sm" variant="danger" @click="$store.commit('click124')" >เอาออกรถเข็น </b-button>
+   </b-col>
+</b-col>
+<b-col>
+  <p>Astolfo  <code>15$</code>:</p>
+ <b-button class="p-3 mb-2 bg-white text-dark" @click="$store.commit('click55')" >
+    <b-img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpCVVIzqyP3u7peTztanjMTuCC-rEF8jhhYA&usqp=CAU" width="170" ></b-img></b-button>
+ <b-col ><b-button size="sm" variant="danger" @click="$store.commit('click55')">ลงรถเข็น 
+   <b-badge variant="light">{{$store.state.click5}}</b-badge></b-button> <b-button size="sm" variant="danger" @click="$store.commit('click125')" >เอาออกรถเข็น </b-button>
+   </b-col></b-col>
+<b-col>
+  <p>Osakabehime<code>24$</code>:</p>
+  <b-button class="p-3 mb-2 bg-white text-dark" @click="$store.commit('click66')" >
+    <b-img src="https://www.inside-games.jp/imgs/zoom/793398.jpg"  width="170" ></b-img></b-button>
+ <b-col ><b-button size="sm" variant="danger" @click="$store.commit('click66')" >ลงรถเข็น 
+   <b-badge variant="light">{{$store.state.click6}}</b-badge></b-button>  <b-button size="sm" variant="danger" @click="$store.commit('click126')" >เอาออกรถเข็น </b-button>
+   </b-col>
+  </b-col>
+  </b-row>
+  <br>
+  <h4><b-icon icon="cart4" animation="spin"  ></b-icon> รายการสินค้าที่สั่งใว้ <b-icon icon="cart4" animation="spin"  ></b-icon></h4>
+    <h5>ราคาของทั้งหมด = {{$store.state.click7}} $</h5>
+    <b-col>
+    <b-button size="sm" variant="danger" @click="$store.commit('click111')" >เอาออกรถเข็นทั้งหมด </b-button> <b-button size="sm" variant="danger"  v-on:click="$store.commit('click1111')" @click="$store.commit('click111')" >ตกลง</b-button></b-col>
+   </div>
+  
+
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+ 
 
-export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
-};
 </script>
